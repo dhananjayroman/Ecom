3d374a25f1156629f22a61d3008adef017e88cae
Dhananjay Roman <romandhananjay01@gmail.com>
2025-06-23 12:43:02 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ function LoginForm() {
 
     try {
       // eslint-disable-next-line no-unused-vars
-      const response = await axios.post("https://ecom-arhe.onrender.com/login", {
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/login`, {
         phoneNumber: phone,
       });
 
